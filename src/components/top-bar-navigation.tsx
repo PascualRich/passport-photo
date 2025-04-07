211340009fa08f64783ff30dd6d59df402dd8ab1
PascualRich <richcruzpascual@gmail.com>
2025-04-07 18:33:10 +0800

    Updates: - add image assets for passport and visa photo covers - update layout and styles - introduce Button, Dropdown, Navigation component - added TopBarNavigation

---
+++ src/components/top-bar-navigation.tsx
@@ -0,0 +1,223 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import React, { useEffect, useRef, useState } from "react";
+import { Button } from "./ui/button";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "./ui/navigation-menu";
+
+interface LinkProps {
+  title: string;
+  width?: string;
+  items: Array<{
+    title: string;
+    href: string;
+    imageUrl?: string;
+  }>;
+  itemsSuffix?: React.ReactNode;
+}
+
+const links: LinkProps[] = [
+  {
+    title: "Resources",
+    items: [
+      { title: "Passport Photo Resizer", href: "resources/passport-photo-resizer" },
+      { title: "How to Take a Passport Photo with an iPhone", href: "/how-to-take-a-passport-photo-with-an-iphone" },
+      { title: "How to Take a Passport Photo with Android", href: "/how-to-take-a-passport-photo-with-android" },
+    ],
+  },
+  {
+    title: "Popular Documents",
+    width: "600px",
+    items: [
+      {
+        title: "Baby Passport Photo",
+        href: "/popular-documents/baby-passport-photo",
+        imageUrl: "/images/US_passport_photo_cover_f9064c684a.webp",
+      },
+      {
+        title: "USCIS Photo",
+        href: "/popular-documents/uscis-photo",
+        imageUrl: "/images/USCIS_photo_cover_7528ad6aa5.webp",
+      },
+      {
+        title: "2x2 Photo",
+        href: "/popular-documents/2x2-photo",
+        imageUrl: "/images/US_passport_photo_cover_f9064c684a.webp",
+      },
+      {
+        title: "Chinese Visa Photo",
+        href: "/popular-documents/chinese-visa-photo",
+        imageUrl: "/images/chinese_visa_photo_cover_90fecd5334.webp",
+      },
+    ],
+    itemsSuffix: (
+      <div className="bg-[#f9f6fe] flex p-5 rounded-md gap-5">
+        <div className="flex flex-col justify-between">
+          <div>
+            <span className="bg-orange-500 text-white font-semibold text-[10px] px-3 mb-5 flex-shrink-0">
+              Most Popular
+            </span>
+          </div>
+          <div>
+            <span className="font-semibold">US Passport Photo</span>
+            <div className="flex gap-2">
+              <span className="text-[12px] text-gray-500">Size</span>
+              <span className="text-[12px] font-semibold">2x2 in</span>
+            </div>
+          </div>
+          <Button variant="default" className="text-[10px] focus-outline-0">
+            Choose document
+          </Button>
+        </div>
+        <Image src="/images/US_passport_photo_cover_f9064c684a.webp" alt="US Passport Photo" width={105} height={150} />
+      </div>
+    ),
+  },
+  {
+    title: "How it Works",
+    items: [
+      { title: "How to Take a Photo", href: "how-it-works/how-to-take-a-photo" },
+      { title: "AI and Expert Verification", href: "how-it-works/ai-and-expert-verification" },
+      { title: "Guarantee", href: "/how-it-works/guarantee" },
+      { title: "Delivery", href: "/how-it-works/delivery" },
+    ],
+  },
+  {
+    title: "About",
+    items: [
+      { title: "About Us", href: "/about" },
+      { title: "Editorial Process", href: "/about/editorial-process" },
+      { title: "Contact", href: "/contact" },
+    ],
+  },
+];
+
+export default function TopBarNavigation() {
+  const headerRef = useRef<HTMLDivElement>(null);
+  const [headerHover, setHeaderHover] = useState(false);
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // State to track if the menu is open
+
+  const setHeaderHoverState = (state: boolean) => {
+    if (!state) setIsMenuOpen(false); // Close menu when mouse leaves
+    if (scrollPosition > 50) return;
+    setHeaderHover(state);
+  };
+
+  useEffect(() => {
+    // Function to handle scroll events
+    const handleScroll = () => {
+      setScrollPosition(window.scrollY); // Update scroll position
+    };
+
+    // Add event listener for scroll
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup the event listener on unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    // Check if scroll position is greater than 100, and update hover state
+    if (isMenuOpen) return; // Don't change hover state if menu is open
+    if (scrollPosition > 50) {
+      setHeaderHover(true);
+    } else {
+      setHeaderHover(false);
+    }
+  }, [scrollPosition]);
+
+  return (
+    <>
+      <header
+        ref={headerRef}
+        onMouseEnter={() => setHeaderHoverState(true)}
+        onMouseLeave={() => setHeaderHoverState(false)}
+        className={cn("w-full fixed z-20", {
+          "bg-white": headerHover,
+          "bg-transparent": !headerHover,
+        })}
+      >
+        <div className="container mx-auto flex items-center justify-between max-w-[80vw]">
+          <div className="flex items-center gap-4">
+            <Image src="/icons/logo.svg" alt="Logo" width={151} height={27} className="h-7 w-auto" />
+
+            {links.map((link, index) => (
+              <NavigationMenu key={link.title}>
+                <NavigationMenuList>
+                  <NavigationMenuItem>
+                    <NavigationMenuTrigger
+                      className="bg-transparent py-10 font-medium border-b-3 border-b-transparent hover:bg-none hover:border-b-primary hover:text-primary rounded-none text-[16px] cursor-pointer"
+                      onMouseEnter={() => setIsMenuOpen(true)} // Open menu on hover
+                    >
+                      {link.title}
+                    </NavigationMenuTrigger>
+                    <NavigationMenuContent className="flex" style={{ width: link.width || "300px" }}>
+                      <ul className={"grid gap-3 p-4"}>
+                        {link.items.map((item) => (
+                          <ListItem key={item.title} title={item.title} href={item.href} imageUrl={item?.imageUrl} />
+                        ))}
+                      </ul>
+                      {link.itemsSuffix && <div className="p-4">{link.itemsSuffix}</div>}
+                    </NavigationMenuContent>
+                  </NavigationMenuItem>
+                </NavigationMenuList>
+              </NavigationMenu>
+            ))}
+          </div>
+          {headerHover && (
+            <Button variant="default" className="text-[16px] focus-outline-0">
+              Choose document
+            </Button>
+          )}
+        </div>
+      </header>
+
+      {/* Black Overlay */}
+      <div
+        className={cn("fixed top-0 left-0 w-full h-full bg-black z-10 transition-opacity", {
+          "opacity-0": !isMenuOpen,
+          "opacity-50": isMenuOpen,
+        })}
+      />
+    </>
+  );
+}
+
+const ListItem = React.forwardRef<React.ElementRef<"a">, React.ComponentPropsWithoutRef<"a"> & { imageUrl?: string }>(
+  ({ className, title, imageUrl, children, ...props }, ref) => {
+    return (
+      <li>
+        <NavigationMenuLink asChild>
+          <a
+            ref={ref}
+            className={cn(
+              "flex flex-row justify-items items-center block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+              className
+            )}
+            {...props}
+          >
+            {imageUrl && (
+              <div className="w-[26px] h-[36px] relative">
+                <Image src={imageUrl} alt={title || ""} layout="fill" objectFit="cover" className=" " />
+              </div>
+            )}
+            <div className="text-[16px] font-medium leading-none">{title}</div>
+          </a>
+        </NavigationMenuLink>
+      </li>
+    );
+  }
+);
+
+ListItem.displayName = "ListItem";
